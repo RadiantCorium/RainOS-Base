da566f376f2d9e5718abf12158c09a97713d2c38
thepercentageguy <chica33sfm@gmail.com>
2023-04-02 22:28:31 +0200

    UMS code works i think maybe idk i didn't test it yet

--- RainOS/core/objects/User.cs
+++ RainOS/core/objects/User.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace RainOS.core.objects
 {
-    internal interface User
+    internal class User
     {
+        /// <summary>
+        /// Creates a new user object
+        /// </summary>
+        /// <param name="name">username</param>
+        /// <param name="password">hashed password</param>
+        internal User(string name, string password)
+        {
+            this.Name = name;
+            this.Password = password;
+        }
+
         public string Name { get; set; }
         public string Password { get; set; }
         public PermissionLevel PermissionLevel { get; set; }
--- RainOS/core/services/UMS.cs
+++ RainOS/core/services/UMS.cs
@@ -1,6 +1,8 @@
-﻿using RainOS.core.objects;
+﻿using RainOS.core.crypto;
+using RainOS.core.objects;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +21,34 @@ namespace RainOS.core.services
         /// This method logs a user in, and is responsible for checking credentials
         /// </summary>
         /// <param name="username">username of the user to log in</param>
-        /// <param name="password">hashed password of the user to log in</param>
-        public static void LoginUser(string username, string password)
+        /// <param name="password">unhashed password of the user to log in</param>
+        /// <returns>0 if sucessful, 1 if user not found, 2 if password incorrect</returns>
+        public static int LoginUser(string username, string password)
         {
+            // hash the password
+            string hashedPass = HashPassword(password);
 
+            // check if the user exists
+            string userInfo = GetUserInfo(username);
+            if (userInfo == null)
+                return 1;
+
+            // compare the credentials
+            if (userInfo.Split(",")[1] != hashedPass)
+                return 2;
+
+            // if we're at this point, the credentials are correct. create the user object and add it
+            User user = new User(username, hashedPass);
+            if (int.TryParse(userInfo.Split(",")[2], out int level))
+            {
+                user.PermissionLevel = (PermissionLevel)level;
+                Users.Add(user);
+            }
+            else
+            {
+                user.PermissionLevel = PermissionLevel.User;
+            }
+            return 0;
         }
 
         /// <summary>
@@ -31,19 +57,99 @@ namespace RainOS.core.services
         /// <param name="user">the user to log out</param>
         public static void LogoutUser(User user)
         {
+            // TODO: add process closing
 
+            Users.Remove(user);
+        }
+
+        /// <summary>
+        /// Closes all applications related to a user, and logs them out
+        /// </summary>
+        /// <param name="name">the user to log out</param>
+        public static void LogoutUser(string name)
+        {
+            // TODO: add process closing
+
+            foreach (User user in Users)
+            {
+                if (user.Name == name)
+                {
+                    Users.Remove(user);
+                }
+            }
         }
 
         /// <summary>
         /// Creates a user
         /// </summary>
         /// <param name="username">username of the user to create</param>
-        /// <param name="password">hashed password of the user</param>
+        /// <param name="password">unhashed password of the user</param>
         /// <param name="permissionLevel">permission level of the user</param>
-        /// <param name="login">Whether or not to automatically log the user in</param>
-        public static void CreateUser(string username, string password, PermissionLevel permissionLevel, bool login)
+        /// <param name="login">Whether to automatically log the user in</param>
+        /// <returns>0 if successfull, 1 if user already exists</returns>
+        public static int CreateUser(string username, string password, PermissionLevel permissionLevel, bool login)
         {
+            // first check if the user already exists
+            if (GetUserInfo(username) != null)
+            {
+                if (login)
+                {
+                    LoginUser(username, password);
+                }
+                return 1;
+            }
 
+            // hash the password
+            string hashedPass = HashPassword(password);
+
+            // if not, write the user's data to the file
+            string userdata = username + "," + hashedPass + "," + (int)permissionLevel;
+
+            StreamWriter s = File.AppendText(@"0:\users.txt");
+            s.WriteLine(userdata);
+            s.Close();
+
+            if (login)
+            {
+                LoginUser(username, password);
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Checks if a user exists
+        /// </summary>
+        /// <param name="username">the username to check for</param>
+        /// <returns>null if the user is not found, otherwise returns user info in the format NAME,HASHEDPASSWORD,PERMISSIONLEVEL</returns>
+        public static string GetUserInfo(string username)
+        {
+            // Load the contents of the "users.txt" file
+            // check if the users.txt file exists, create one if not
+            if (!File.Exists(@"0:\users.txt"))
+            {
+                File.Create(@"0:\users.txt");
+            }
+            string[] usersFile = File.ReadAllText(@"0:\users.txt").Split("\n");
+
+            foreach (string user in usersFile)
+            {
+                string name = user.Split(",")[0];
+                if (name == username)
+                {
+                    return user;
+                }
+            }
+
+            return null;
+        }
+
+        public static string HashPassword(string password)
+        {
+            Sha256 sha256 = new Sha256();
+            byte[] passwordBytesUnhashed = Encoding.Unicode.GetBytes(password);
+            sha256.AddData(passwordBytesUnhashed, 0, (uint)passwordBytesUnhashed.Length);
+            return Convert.ToBase64String(sha256.GetHash());
         }
     }
 }
